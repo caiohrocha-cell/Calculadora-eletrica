14e62bfc2237b48e101fb8303f47871309232fb0
caiohrocha-cell <caio_h_rocha@estudante.sesisenai.org.br>
2026-03-06 19:22:13 -0300

    Conversão de unidade para MULTI
    Implementação

---
+++ calculadormultiplo&Sub.js
@@ -0,0 +1,11 @@
+//CALCULADORA DE MULTI E SUB
+ampere();
+function ampere(){
+let a = ampere
+let unidade = parseFloat(prompt("Escolha a grandeza elétrica:"));
+let valorNumerico = parseFloat(prompt("Insira o valor númerico da médida:"));
+let conversAo = parseFloat(prompt("Tipo de conversão desejada:"));
+
+let (a * 0.001)
+
+}
